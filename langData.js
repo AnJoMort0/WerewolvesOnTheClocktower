f096d2d93eaee54c79e8380101a68617c277a239
AnJoMort0 <andrejsfonseca@gmail.com>
2026-02-15 15:17:45 +0100

    Added base table

---
+++ langData.js
@@ -0,0 +1,63 @@
+const langData = {
+    PT: {
+        //GENERAL
+        title:          "Lobisomens na Torre Sangrenta",
+        numPlaceholder: "Numero de Jogadores (min 8)",
+        createTable:    "Criar Tabela",
+        numPlayerError: "Por favor, insira um número, no mínimo 8",
+        player:         "Jogador",
+
+        //CHARACTERS
+        characters: [
+            "e01. Lobisomem",
+            "e02. Bruxa Malvada",
+            "e03. Chaman",
+            "e04. Vidente",
+
+            "v01. Menina",
+            "v02. Domador do Urso",
+            "v03. Domador do Corvo",
+            "v04. Domador da Raposa",
+            "v05. Domador dos Coelhos",
+            "v06. Marionetista",
+            "v07. Cavaleiro Enferrujado",
+            "v08. Caçador",
+            "v08b. Capuchinho Vermelho",
+            "v09. Capitão",
+            "v10. Paranoico",
+            "v11. Chefe da Aldeia",
+            "v12. Cigana",
+            "v13. Juiz",
+            "v14. Acusador",
+            "v15. Piromaníaco",
+            "v16. Sonâmbulo",
+            "v17. Salvador",
+            "v18. Anjo",
+            "v19. Profeta",
+
+            "m01. Lobisomem Mau",
+            "m02. Lobisomem Vidente",
+            "m03. Lobisomem Vampiro",
+            "m04. Ankou",
+            "m05. Cupido Malvado",
+
+            "s01. Cupido",
+            "s02. Lobisomem Branco",
+
+            "f01. Ladrão",
+
+            "a01. Bêbado",
+            "a02. Cão-Lobo",
+            "a03. Mimo",
+            "a04. Ator",
+            "a05. Substituto",
+            "a06. Ilusionista",
+            "a07. Amante Secreto",
+
+            "l01. Aldeão Triste",
+            "l02. Criança Selvagem",
+            "l03. Irmãs",
+            "l04. Irmãos",
+        ]
+    }
+}
